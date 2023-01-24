418cbcf1989387917d376ed6aecc4402a9ab6717
Jedrzej99 <118077779+Jedrzej99@users.noreply.github.com>
2023-01-24 01:29:38 +0100

    Add files via upload

---
+++ Human.java
@@ -0,0 +1,12 @@
+public class Human {
+    public Animal pet;
+    public String name;
+    public String surname;
+    public Car car;
+
+    public Car getCar() {
+
+        return this.car;
+    }
+
+}
